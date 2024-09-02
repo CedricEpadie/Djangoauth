e6a74e80abf2289d4852655cd071ba3fd98c4dde
CedricEpadie <oceane.epadie@gmail.com>
2024-09-02 22:34:48 +0100

    Ajout des fichiers templates et du base.html

--- siteprod/urls.py
+++ siteprod/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.acceuil),
+    path('auth/', include('auth.urls')),
 ]
