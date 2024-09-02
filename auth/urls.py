48c924e386f37239f84a639c7d05ea47fdc51b45
CedricEpadie <oceane.epadie@gmail.com>
2024-09-02 22:18:59 +0100

    Ajout des urls de 'auht' et configuration de la vue de l'inscription

--- auth/urls.py
+++ auth/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'auth'
 
 urlpatterns = [
-    path('', views)
+    path('', views.acceuil, name='acceuil'),
+    path('inscription/', views.inscription, name='inscription'),
+    path('connexion/', views.connexion, name='connexion'),
+    path('deconnexion/', views.deconnexion, name='deconnexion'),
+    path('profil/', views.profil, name='profil'),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def inscription(request):
-    return render(request, 'auth/inscription.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('auth:profil')
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'auth/inscription.html', {'form' : form})
 
 def connexion(request):
     return render(request, 'auth/connexion.html')
